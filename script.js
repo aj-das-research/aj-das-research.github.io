d650885582a1d2a3700e1d51fbbd484662ee2a8c
aj-das-research <aj.das.research@gmail.com>
2024-09-01 23:27:07 +0530

    Initial commit 6

--- script.js
+++ script.js
@@ -6,3 +6,39 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Simple slider functionality
+const slider = (selector) => {
+    const sliderContainer = document.querySelector(selector);
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    sliderContainer.addEventListener('mousedown', (e) => {
+        isDown = true;
+        sliderContainer.classList.add('active');
+        startX = e.pageX - sliderContainer.offsetLeft;
+        scrollLeft = sliderContainer.scrollLeft;
+    });
+
+    sliderContainer.addEventListener('mouseleave', () => {
+        isDown = false;
+        sliderContainer.classList.remove('active');
+    });
+
+    sliderContainer.addEventListener('mouseup', () => {
+        isDown = false;
+        sliderContainer.classList.remove('active');
+    });
+
+    sliderContainer.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - sliderContainer.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        sliderContainer.scrollLeft = scrollLeft - walk;
+    });
+};
+
+slider('.collaborator-slider');
+slider('.mentor-slider');
